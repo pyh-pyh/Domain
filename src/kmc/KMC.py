3e6cc913040a8635a472b7acb17d3cf52e6ff3a0
pyh-pyh <2335257900@qq.com>
2021-02-04 22:51:59 +0800

    2021.2.4

--- src/kmc/KMC.py
+++ src/kmc/KMC.py
@@ -16,6 +16,8 @@ class KMC:
     CHARACTERISTIC_LENGTH = 0
     SIMULATION_TIME = 0
     TEMPERATURE = 0
+    DEFECT = None
+    INITIAL_DEFECT_TYPE = None
 
     def set_up_box(self, shape:str, L:float):
         self.BOX_SHAPE = shape
@@ -36,7 +38,9 @@ class KMC:
             pass
 
     def read_defect_data(self, filename):
-        defect_data = np.loadtxt('data/' + filename, dtype = [('ID', 'i4'), ('type', np.str, 10), ('x', 'f4'), ('y', 'f4'), ('z', 'f4')], skiprows = 1)
+        raw_defect_data = np.loadtxt('data/' + filename, dtype = [('ID', 'i4'), ('type', np.str, 10), ('x', 'f4'), ('y', 'f4'), ('z', 'f4')], skiprows = 1)
+        defect_data = self.encode_defect(raw_defect_data)
+        self.DEFECT = defect_data
         return defect_data
 
     def generate_defect_data(self, N, defectype = ['I', 'V']):
@@ -58,6 +62,28 @@ class KMC:
                 print('%d %s %f %f %f'%(i+1, defectype[flag], x*bound[1], y*bound[3], z*bound[5]), file = f)
         return self.read_defect_data(indexedfilename)
 
+    def encode_defect(self, raw_defect_data):
+        initial_defect_type = {}
+        defect_type_serial = 1
+        for i in raw_defect_data:
+            if i[1] not in initial_defect_type.keys():
+                initial_defect_type[i[1]] = defect_type_serial
+                defect_type_serial += 1
+        self.INITIAL_DEFECT_TYPE = initial_defect_type
+        defect_data = np.empty((raw_defect_data.shape[0], 5), dtype = np.float16)
+        for i in range(len(defect_data)):
+            for j in range(len(defect_data[i])):
+                if j == 1:
+                    defect_data[i][j] = initial_defect_type[raw_defect_data[i][j]]
+                else:
+                    defect_data[i][j] = raw_defect_data[i][j]
+        print(defect_data, defect_data.shape)
+        return defect_data
+
+        
+
+        
+
     def simulation(self):
         t = 0
         while t <= self.TEMPERATURE:
@@ -67,4 +93,8 @@ class KMC:
         pass
 
 if __name__ == '__main__':
+    simulator = KMC()
+    simulator.set_up_box('cube', 10)
+    data = simulator.generate_defect_data(100)
+    print(data.shape)
 
